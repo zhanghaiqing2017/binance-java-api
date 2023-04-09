f39e760e15b1435597da03e0d0fafe7d81669d60
ICPLAZA01 <1851181931@qq.com>
2023-04-09 10:50:33 +0800

    1

--- src/main/java/com/binance/api/client/Test.java
+++ src/main/java/com/binance/api/client/Test.java
@@ -14,12 +14,6 @@ public class Test {
         return client;
     }
     public static void main(String[] args) {
-        BinanceApiMarginRestClient client =creatBinanceApiMarginRestClient("66dLHliynTUBTTPymeoZkUXQQMJWMT2mZ4hc4jeEg7hksgPjEC6NLGshrlrRfqZG",
-                "9b32ni7aI2qTGK32g51hvLK1vkekGZAKrVET1vOZvHWJ3JwyDqQDOmJq0UStZdOl");
-        MarginTransaction usdt = client.subToMaster("USDT", "1");
-
-
-        System.out.println(usdt.getTranId());
 
     }
 }
